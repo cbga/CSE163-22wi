64495fbb7d45c1bf097e685e0036bbe87acc7db1
cnbga <cnbga@outlook.com>
2022-01-06 19:12:30 -0800

    update hw0.py

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -1,12 +1,22 @@
+"""
+Implements the functions for HW0
+"""
+
+
 def funky_sum(a, b, mix):
-    return (1 - mix) * b + mix * a
+    if mix <= 0:
+        return a
+    elif mix >= 1:
+        return b
+    else:
+        return (1 - mix) * a + mix * b
 
 
 def total(n):
     if n < 0:
-        return None;
+        return None
     else:
-        result = 0;
+        result = 0
         for i in range(n + 1):
-            result += i;
-        return result;
+            result += i
+        return result
