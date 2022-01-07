db4c8ff31a73631954d527bd68a899a5c33decca
cnbga <cnbga@outlook.com>
2022-01-06 20:20:35 -0800

    update hw0

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -4,6 +4,15 @@ Implements the functions for HW0
 
 
 def funky_sum(a, b, mix):
+    """
+    :param a: the first number
+    :param b: the second number
+    :param mix: control the return value
+    :return: if mix is less or equal to 0, return a; if mix is larger or
+            equal to 1, return b; if mix is between 0 and 1 (exclusively),
+            return the sum if 1-mix times a and mix times b
+            (linear interpolation).
+    """
     if mix <= 0:
         return a
     elif mix >= 1:
@@ -13,6 +22,11 @@ def funky_sum(a, b, mix):
 
 
 def total(n):
+    """
+    :param n: number use to be the uppper bound of adding value
+    :return: if n is negative, return None value; if not, return
+            the sum of all integers from 0 to n (inclusively).
+    """
     if n < 0:
         return None
     else:
@@ -20,3 +34,21 @@ def total(n):
         for i in range(n + 1):
             result += i
         return result
+
+
+def swip_swap(source, c1, c2):
+    """
+    :param source: the target string to be processed
+    :param c1: the first swapping character
+    :param c2: the second swapping character
+    :return: a copy of source with all occurrences of c1 and c2 swapped
+    """
+    result = ''
+    for i in source:
+        if i == c1:
+            result += c2
+        elif i == c2:
+            result += c1
+        else:
+            result += i
+    return result
--- hw0/hw0_test.py
+++ hw0/hw0_test.py
@@ -1,9 +1,26 @@
-import hw0
+"""
+Tests the methods (behaviors) for hw0.py
+"""
 
+import hw0
 from cse163_utils import assert_equals
 
 
+def test_funky_sum():
+    """
+    Test the method funky_sum(a, b, mix) in hw0.py
+    """
+    assert_equals(2.0, hw0.funky_sum(1, 3, 0.5))
+    assert_equals(1, hw0.funky_sum(1, 3, 0))
+    assert_equals(1.5, hw0.funky_sum(1, 3, 0.25))
+    assert_equals(5, hw0.funky_sum(2, 5, 2))
+    assert_equals(3.6, hw0.funky_sum(3, 4, 0.6))
+
+
 def test_total():
+    """
+    Test the method total(n) in hw0.py
+    """
     # The regular case
     assert_equals(15, hw0.total(5))
     # Seems likely we could mess up 0 or 1
@@ -11,10 +28,22 @@ def test_total():
     assert_equals(0, hw0.total(0))
 
 
+def test_swip_swap():
+    """
+    Test the method swip_swap(source, c1, c2) in hw0.py
+    """
+    assert_equals('offbar', hw0.swip_swap('foobar', 'f', 'o'))
+    assert_equals('foocar', hw0.swip_swap('foobar', 'b', 'c'))
+    assert_equals('foobar', hw0.swip_swap('foobar', 'z', 'c'))
+    assert_equals('hallo', hw0.swip_swap('hello', 'e', 'a'))
+    assert_equals('monrirg', hw0.swip_swap('morning', 'r', 'n'))
+
+
 def main():
     test_total()
+    test_funky_sum()
+    test_swip_swap()
 
 
 if __name__ == '__main__':
     main()
-
