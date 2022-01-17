75e34cf6e675652eca58f8e92e34c95fbe5b7804
cnbga <cnbga@outlook.com>
2022-01-17 04:01:36 -0800

    update hw1 tests (all passed no comment)

--- hw1/hw1_test.py
+++ hw1/hw1_test.py
@@ -17,8 +17,78 @@ def test_total():
     assert_equals(None, hw1.total(-1))
 
 
+def test_count_divisible_digits():
+    assert_equals(4, hw1.count_divisible_digits(650899, 3))
+    assert_equals(1, hw1.count_divisible_digits(-204, 5))
+    assert_equals(0, hw1.count_divisible_digits(24, 5))
+    assert_equals(0, hw1.count_divisible_digits(1, 0))
+    # additional
+    assert_equals(5, hw1.count_divisible_digits(99999, 3))
+    assert_equals(2, hw1.count_divisible_digits(600, 8))
+
+
+def test_is_relatively_prime():
+    assert_equals(True, hw1.is_relatively_prime(12, 13))
+    assert_equals(False, hw1.is_relatively_prime(12, 14))
+    assert_equals(True, hw1.is_relatively_prime(5, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 1))
+    # additional
+    assert_equals(False, hw1.is_relatively_prime(81, 72))
+    assert_equals(True, hw1.is_relatively_prime(23, 31))
+
+
+def test_travel():
+    assert_equals((-1, 4), hw1.travel('NW!ewnW', 1, 2))
+    # additional
+    assert_equals((2, 3), hw1.travel('!!!!!!00022!?', 2, 3))
+    assert_equals((4, 2), hw1.travel('eeeEnnN!s', 0, 0))
+
+
+def test_reformat_date():
+    assert_equals("3/1/2", hw1.reformat_date('1/2/3', 'M/D/Y', 'Y/M/D'))
+    assert_equals("4/0", hw1.reformat_date('0/200/4', 'Y/D/M', 'M/Y'))
+    assert_equals("2", hw1.reformat_date('3/2', 'M/D', 'D'))
+    assert_equals("31/12/1998", hw1.reformat_date("12/31/1998", "M/D/Y", "D/M/Y"))
+    # additional
+    assert_equals("1/17/2022", hw1.reformat_date('2022/1/17', 'Y/M/D', 'M/D/Y'))
+    assert_equals("1", hw1.reformat_date('2022/1/17', 'Y/M/D', 'M'))
+
+
+def test_longest_word():
+    assert_equals('3: Merrily,', hw1.longest_word('song.txt'))
+    # additional
+    assert_equals('13: beautiful!', hw1.longest_word('second_song.txt'))
+    assert_equals('6: master,', hw1.longest_word('third_song.txt'))
+
+
+def test_get_average_in_range():
+    assert_equals(5.5, hw1.get_average_in_range([1, 5, 6, 7, 9], 5, 7))
+    assert_equals(2.0, hw1.get_average_in_range([1, 2, 3], -1, 10))
+    # additional
+    assert_equals(0, hw1.get_average_in_range([], 1, 100))
+    assert_equals(-2.0, hw1.get_average_in_range([-1, -3, -5, 87], -4, 0))
+
+
+def test_mode_digit():
+    assert_equals(1, hw1.mode_digit(12121))
+    assert_equals(0, hw1.mode_digit(0))
+    assert_equals(2, hw1.mode_digit(-122))
+    assert_equals(2, hw1.mode_digit(1211232231))
+    # additional
+    assert_equals(0, hw1.mode_digit(100000))
+    assert_equals(9, hw1.mode_digit(-8967509988))
+
+
 def main():
     test_total()
+    test_mode_digit()
+    test_travel()
+    test_get_average_in_range()
+    test_longest_word()
+    test_count_divisible_digits()
+    test_reformat_date()
+    test_is_relatively_prime()
     # Call your test functions here!
 
 
