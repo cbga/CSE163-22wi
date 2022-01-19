e6e035f6b368d9093e99eb5f828cc69ec53d7cd4
cnbga <cnbga@outlook.com>
2022-01-18 23:52:10 -0800

    update hw1 comments

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,3 +1,9 @@
+"""
+Bingan Chen (AA)
+Implements the function for hw1 Primer.
+"""
+
+
 def total(n):
     """
     Returns the sum of the numbers from 0 to n (inclusive).
@@ -14,6 +20,10 @@ def total(n):
 
 # Write your functions here!
 def count_divisible_digits(n, m):
+    """
+    Returns the number of digits in "n" that are divisible by "m"
+    (0 <= m <= 10).
+    """
     if m == 0:
         return 0
     if n <= 0:
@@ -28,13 +38,20 @@ def count_divisible_digits(n, m):
 
 
 def is_relatively_prime(n, m):
-    for i in range(2, n):
+    """
+    Returns if the numbers "n" and "m" share no common factors besides 1.
+    """
+    for i in range(2, n + 1):
         if m % i == 0 and n % i == 0:
             return False
     return True
 
 
 def travel(direction, x, y):
+    """
+    Returns a tuple indicating the new position after following the
+    "direction" starting from "x", "y".
+    """
     x_new = x
     y_new = y
     for i in range(len(direction)):
@@ -51,6 +68,10 @@ def travel(direction, x, y):
 
 
 def reformat_date(date, current, target):
+    """
+    Returns a new string with the "date" formatted in the "target" format
+    from the "current" format.
+    """
     date_list = date.split('/')
     current_list = current.split('/')
     target_list = target.split('/')
@@ -64,34 +85,47 @@ def reformat_date(date, current, target):
 
 
 def longest_word(file_name):
+    """
+    Returns the longest word in the file ("file_name") with which
+    line it appears on.
+    If the file is empty or there are no words in the file, returns None.
+    """
     line_num = 0
     max_line = 0
-    longest_word = ''
+    the_longest = ''
     with open(file_name) as f:
         for line in f.readlines():
             line_num += 1
             for word in line.split():
-                if len(word) > len(longest_word):
+                if len(word) > len(the_longest):
                     max_line = line_num
-                    longest_word = word
-    if longest_word == '':
+                    the_longest = word
+    if the_longest == '':
         return None
-    return str(max_line) + ': ' + longest_word
+    return str(max_line) + ': ' + the_longest
 
 
 def get_average_in_range(li, low, high):
-    if len(li) == 0:
-        return 0
+    """
+    Returns the average of all values in list "li" in the range from "low"
+    (inclusive) to "high" (exclusive).
+    If there are no values in the given range, returns 0.
+    """
     sum_num = 0
     count = 0
     for i in range(low, high):
         if i in li:
             sum_num += i
             count += 1
+    if count == 0:
+        return 0
     return sum_num / count
 
 
 def mode_digit(n):
+    """
+    Returns the digit that appears most frequently in the number "n".
+    """
     if n == 0:
         return 0
     n = abs(n)
--- hw1/hw1_test.py
+++ hw1/hw1_test.py
@@ -1,3 +1,8 @@
+"""
+Bingan Chen (AA)
+Tests functions in hw1 (hw1.py).
+"""
+
 import hw1
 
 from cse163_utils import assert_equals
@@ -18,6 +23,9 @@ def test_total():
 
 
 def test_count_divisible_digits():
+    """
+    Tests the count_divisible_digits method
+    """
     assert_equals(4, hw1.count_divisible_digits(650899, 3))
     assert_equals(1, hw1.count_divisible_digits(-204, 5))
     assert_equals(0, hw1.count_divisible_digits(24, 5))
@@ -28,6 +36,9 @@ def test_count_divisible_digits():
 
 
 def test_is_relatively_prime():
+    """
+    Tests the is_relatively_prime method
+    """
     assert_equals(True, hw1.is_relatively_prime(12, 13))
     assert_equals(False, hw1.is_relatively_prime(12, 14))
     assert_equals(True, hw1.is_relatively_prime(5, 9))
@@ -35,10 +46,13 @@ def test_is_relatively_prime():
     assert_equals(True, hw1.is_relatively_prime(8, 1))
     # additional
     assert_equals(False, hw1.is_relatively_prime(81, 72))
-    assert_equals(True, hw1.is_relatively_prime(23, 31))
+    assert_equals(False, hw1.is_relatively_prime(3, 3))
 
 
 def test_travel():
+    """
+    Tests the travel method
+    """
     assert_equals((-1, 4), hw1.travel('NW!ewnW', 1, 2))
     # additional
     assert_equals((2, 3), hw1.travel('!!!!!!00022!?', 2, 3))
@@ -46,16 +60,24 @@ def test_travel():
 
 
 def test_reformat_date():
+    """
+    Tests the reformat_date method
+    """
     assert_equals("3/1/2", hw1.reformat_date('1/2/3', 'M/D/Y', 'Y/M/D'))
     assert_equals("4/0", hw1.reformat_date('0/200/4', 'Y/D/M', 'M/Y'))
     assert_equals("2", hw1.reformat_date('3/2', 'M/D', 'D'))
-    assert_equals("31/12/1998", hw1.reformat_date("12/31/1998", "M/D/Y", "D/M/Y"))
+    assert_equals("31/12/1998", hw1.reformat_date(
+        "12/31/1998", "M/D/Y", "D/M/Y"))
     # additional
-    assert_equals("1/17/2022", hw1.reformat_date('2022/1/17', 'Y/M/D', 'M/D/Y'))
+    assert_equals("1/17/2022", hw1.reformat_date(
+        '2022/1/17', 'Y/M/D', 'M/D/Y'))
     assert_equals("1", hw1.reformat_date('2022/1/17', 'Y/M/D', 'M'))
 
 
 def test_longest_word():
+    """
+    Tests the longest_word method
+    """
     assert_equals('3: Merrily,', hw1.longest_word('song.txt'))
     # additional
     assert_equals('13: beautiful!', hw1.longest_word('second_song.txt'))
@@ -63,14 +85,23 @@ def test_longest_word():
 
 
 def test_get_average_in_range():
+    """
+
+    Tests the get_average_in_range method
+    """
     assert_equals(5.5, hw1.get_average_in_range([1, 5, 6, 7, 9], 5, 7))
     assert_equals(2.0, hw1.get_average_in_range([1, 2, 3], -1, 10))
     # additional
     assert_equals(0, hw1.get_average_in_range([], 1, 100))
     assert_equals(-2.0, hw1.get_average_in_range([-1, -3, -5, 87], -4, 0))
+    assert_equals(0, hw1.get_average_in_range([], -4, 0))
+    assert_equals(0, hw1.get_average_in_range([-8, -9], -4, 0))
 
 
 def test_mode_digit():
+    """
+    Tests the mode_digit method
+    """
     assert_equals(1, hw1.mode_digit(12121))
     assert_equals(0, hw1.mode_digit(0))
     assert_equals(2, hw1.mode_digit(-122))
