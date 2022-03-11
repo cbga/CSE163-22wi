3f4c6f91189768bd9042e468524e7e98e8b32c23
ba <cnbga@outlook.com>
2022-03-11 01:02:26 -0800

    u data_process.py

--- Project/data_process.py
+++ Project/data_process.py
@@ -3,14 +3,11 @@ import os
 import nltk
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 from nltk.sentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import requests
 from os.path import exists
 
-os.environ['TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAHJRZQEAAAAA7dJCPjzhW7PhpmKz4EDKtga%2BmEQ%3DkwPq2w9tpjgMbPRYiYsH4rv2GV3tC0hSzP4Fk1WQ1zZ90OZHbt'
-nltk.download('vader_lexicon')
 
 def auth():
     return os.getenv('TOKEN')
@@ -24,7 +21,7 @@ def get_headers(token):
 def get_url(result_num=10, start_date='2022-03-03'):
     url = 'https://api.twitter.com/2/tweets/search/recent'
     params = {
-        "query":['covid -is:reply lang:en'],
+        "query": ['covid -is:reply lang:en'],
         'end_time': start_date + 'T09:25:07.000Z',
         'max_results': result_num,
         'tweet.fields': 'text,created_at',
@@ -33,7 +30,7 @@ def get_url(result_num=10, start_date='2022-03-03'):
     return url, params
 
 
-def connect_to_endpoint(url, headers, params, next_token = None):
+def connect_to_endpoint(url, headers, params, next_token=None):
     params['next_token'] = next_token
     response = requests.request("GET", url, headers=headers, params=params)
     print("Endpoint Response Code: " + str(response.status_code))
@@ -48,7 +45,6 @@ def get_data(tweet_num, start_date):
     target_num = tweet_num
     url, params = get_url(target_num, start_date)
     resp = connect_to_endpoint(url, headers, params)
-    # time.sleep(1)
     return resp
 
 
@@ -63,44 +59,43 @@ def write_data_in_csv(j_content):
             attitude = 'NON_negative'
         else:
             attitude = 'negative'
-        tweet_info = [create_date ,content, attitude]
+        tweet_info = [create_date, content, attitude]
         wt.writerow(tweet_info)
-    
+
 
 def init_csv_writer(path):
     global wt
     wt = csv.writer(path)
     wt.writerow(['created_at', 'text', 'attitude'])
-    
+
 
 def init_csv_file_and_writer(file_name):
     # Last change to input name
     global f
     if not exists(file_name):
-        f = open(file_name, 'x', newline='')
-    f = open(file_name, 'w', newline='')
+        f = open(file_name, 'x', newline='', encoding="utf-8")
+    f = open(file_name, 'w', newline='', encoding="utf-8")
     init_csv_writer(f)
 
 
- # Filter the daily new cases by the same date as the tweets
+# Filter the daily new cases by the same date as the tweets
 def get_cases_by_day(df_cases):
     df_cases = df_cases[['Date_reported', 'New_cases']]
     df_cases = df_cases.groupby(by=['Date_reported']).sum()
-    df_cases = df_cases.loc['2022-03-03':'2022-03-08']
+    df_cases = df_cases.loc['2022-03-04':'2022-03-09']
     return df_cases
 
 
-
-def get_alti_by_day(df_tweet):
-    tweet_inds = df_tweet.index.tolist()
+def get_attitude_by_day(df_tweet):
+    tweet_indexes = df_tweet.index.tolist()
     res_dict = {}
-    for index in tweet_inds: 
+    for index in tweet_indexes:
         if index not in res_dict.keys():
             res_dict[index] = []
-            alts_for_ind_aday = df_tweet.loc[index, 'attitude']
-            for alt in alts_for_ind_aday:
-                res_dict[index].append(alt)
-    # percentage for non negative comments in all for the day
+            attitude_for_day = df_tweet.loc[index, 'attitude']
+            for attitude in attitude_for_day:
+                res_dict[index].append(attitude)
+    # percentage for non-negative comments in all for the day
     for day in res_dict.keys():
         single = res_dict[day]
         neg = 0
@@ -119,31 +114,38 @@ def get_alti_by_day(df_tweet):
 
 def join_attitudes_and_cases(df_attitude, df_cases):
     merged = df_cases.join(df_attitude, how='left')
-    merged = merged.rename(columns={1:'Non-negative Percent'})
+    merged = merged.rename(columns={1: 'Non-negative Percent'})
     return merged
 
 
 def visualize(df_final):
-    sns.lineplot(markers=True, data=df_final, x='New_cases', y='Non-negative Percent')
-    plt.title('Relationship Between Daily New Cases and Non-nagative Tweets about COVID-19', loc='center', wrap=True)
+    sns.lineplot(markers=True, data=df_final, x='New_cases',
+                 y='Non-negative Percent')
+    plt.title(
+        'Relationship Between Daily New Cases and Non-negative Tweets about '
+        'COVID-19',
+        loc='center', wrap=True)
     plt.savefig('relationship.png')
 
 
 def main():
-    df_cases = pd.read_csv('global-data-bycount.csv')
+    os.environ[
+        'TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAHJRZQEAAAAA7dJCPjzhW7PhpmKz4EDKtga' \
+                   '%2BmEQ%3DkwPq2w9tpjgMbPRYiYsH4rv2GV3tC0hSzP4Fk1WQ1zZ90OZHbt'
+    auth()
+    nltk.download('vader_lexicon')
+    df_cases = pd.read_csv('global-data-by-country-dat.csv')
     df_cases = get_cases_by_day(df_cases)
     csv_name = 'data.csv'
     init_csv_file_and_writer(csv_name)
     for date in df_cases.index.tolist():
         write_data_in_csv(get_data(100, date))
-    print()
     print('... Done writing data!')
-    print()
     f.close()
     df_tweet = pd.read_csv(csv_name)
     df_tweet.set_index('created_at', inplace=True)
-    df_attitude = get_alti_by_day(df_tweet)
-    
+    df_attitude = get_attitude_by_day(df_tweet)
+    # combine two dataframe by their date
     joined_df = join_attitudes_and_cases(df_attitude, df_cases)
     visualize(joined_df)
 
